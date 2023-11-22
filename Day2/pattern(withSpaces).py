c3fb7c3f96bb325b6d6a08f4c5681eb2d16fa077
Daniel-144 <dani0142004@gmail.com>
2023-11-22 21:18:51 +0530

    Day 2

---
+++ Day2/pattern(withSpaces).py
@@ -0,0 +1,49 @@
+"""
+Problem #1
+Print the following pattern
+     1
+    212
+   32123
+  4321234
+ 543212345
+Get user input for how far to go (up to 0)
+"""
+n = int(input("Enter the number of rows (up to 9): "))
+
+for i in range(1, n + 1):
+    # foor loop for sapcing
+    for j in range(n+1,i,-1):
+        print(" ",end="")
+
+    # loop to Print decreasing numbers
+    for k in range(i, 0, -1):
+        print(k, end="")
+    # example OP of the above  loop.
+    """
+    1
+    21
+    321
+    4321
+    """
+    # loop to Print increasing numbers
+    for l in range(2, i + 1):
+        print(l, end="")
+    # to print new line
+    print()
+    # example Op of the above loop .
+    """
+    
+    2
+    23
+    234
+    """
+
+"""
+OP:
+Enter the number of rows (up to 9): 5
+     1
+    212
+   32123
+  4321234
+ 543212345
+"""
